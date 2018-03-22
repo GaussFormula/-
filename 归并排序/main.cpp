fe6f976a2687b7be8f6efd96cd8a2d1f6d2e4cca
翊岚 邓 <c594794@hotmail.com>
2018-03-22 23:18:25 +0800

    添加INT32_MAX当作标志

---
+++ 归并排序/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <limits.h>
+void merge_sort(int*,int,int);
+void merge(int*, int, int,int);
+using namespace std;
+int main()
+{
+	int* source = new int[10];
+	srand(unsigned(time(NULL)));
+	for (int i = 0; i < 10; i++)
+	{
+		source[i] = rand();
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << source[i] << " ";
+	}
+	cout << endl;
+	merge_sort(source, 0, 9);
+	for (int i = 0; i < 10; i++)
+	{
+		cout << source[i] << " ";
+	}
+	cout << endl;
+	getchar();
+	return 0;
+}
+void merge_sort(int*source, int p, int r)
+{
+	int q = (p + r) / 2;
+	if (p<r)
+	{
+		merge_sort(source, p, q);
+		merge_sort(source, q + 1, r);
+	}
+	merge(source, p, q, r);
+}
+void merge(int* source,int p, int q,int r)
+{
+	int LeftLength = q - p + 1;
+	int RightLength = r - q;
+	int* LeftArray = new int[LeftLength+1];
+	int* RightArray = new int[RightLength+1];
+	for (int i = 0; i < LeftLength; i++)
+	{
+		LeftArray[i] = source[p + i];
+	}
+	LeftArray[LeftLength] = INT32_MAX;
+	for (int i = 0; i < RightLength; i++)
+	{
+		RightArray[i] = source[q];
+	}
+	RightArray[RightLength] = INT32_MAX;
+	for (int i = 0,j=0,k=0; k < r; k++)
+	{
+		if (LeftArray[i] <= RightArray[j])
+		{
+			source[k] = LeftArray[i];
+			i++;
+		}
+		else
+		{
+			source[k] = RightArray[j];
+			j++;
+		}
+	}
+}
