d8d43fd59bfc4a16346f3de964411bfa988ea53a
翊岚 邓 <c594794@hotmail.com>
2018-10-08 23:42:13 +0800

    update QuickSort

--- 归并排序/QuickSort.h
+++ 归并排序/QuickSort.h
@@ -1,32 +1,25 @@
 #pragma once
 #include <iostream>
+#include <vector>
 using namespace std;
-void swap(int* a, int* b)
+void QuickSort(vector<int>& source, int length,int Left,int Right)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-void QuickSort(int* source, int length,int Left,int Right)
-{
-	if (Left >= Right)
+	if (Left >= Right)//当左下标等于右下标时，说明数组此时只有一个元素，排序结束
 	{
 		return;
 	}
-	int key = source[Right];
-	int i, j;
+	int key = source[Right];//取尾值做主元
+	int i, j;//临时变量i及其左边存放比主元小的值，右边存放比主元大的值
 	for (i = Left-1,j=Left; j < Right; j++)
 	{
-		if (source[j] < key)
+		if (source[j] < key)//若小于主元
 		{
-			i++;
+			i++;//小于主元的区间扩张
+			swap(source[i], source[j]);//并将新出现的比主元小的数置入进来
 		}
-		else
-		{
-			swap(source + i + 1, source + j);
-		}
+		
 	}
-	swap(source + i + 1, source+Right);
-	QuickSort(source, length, Left, i);
-	QuickSort(source, length, i+2, Right);
+	swap(source[i + 1], source[Right]);//将主元放在两个区间中间
+	QuickSort(source, length, Left, i);//递归求解左区间
+	QuickSort(source, length, i+2, Right);//递归求解右区间
 }
--- 归并排序/main.cpp
+++ 归并排序/main.cpp
@@ -13,7 +13,7 @@ int main()
 {
 	srand((int)time(0));
 	vector<int> v,v1;
-	for (int i = 1; i <= 5; i++)
+	for (int i = 1; i <= 10; i++)
 	{
 		v.push_back(random(100));
 	}
@@ -22,12 +22,18 @@ int main()
 		cout << *it<<" ";
 	}
 	cout << endl;
-	CountingSort(v, v1, 100);
+	//CountingSort(v, v1, 100);
 	//HeapSort(&v, &v1);
-	for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
+	QuickSort(v, v.size(), 0, v.size() - 1);
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
 	{
 		cout << *it << " ";
 	}
+	cout << endl;
+	/*for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
+	{
+		cout << *it << " ";
+	}*/
 	getchar();
 	return 0;
 }
